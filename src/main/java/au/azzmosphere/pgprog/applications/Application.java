6fc6d0879c10c928c7e6fd4f3079e414cba4dd8f
Aaron Spiteri <aaron.spiteri@staff.iinet.net.au>
2017-05-13 23:39:34 +1000

    Removed unused dependancies

--- src/main/java/au/azzmosphere/pgprog/applications/Application.java
+++ src/main/java/au/azzmosphere/pgprog/applications/Application.java
@@ -1,10 +1,8 @@
 package au.azzmosphere.pgprog.applications;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by aaron.spiteri on 13/5/17.
--- src/main/java/au/azzmosphere/pgprog/controllers/PgProgController.java
+++ src/main/java/au/azzmosphere/pgprog/controllers/PgProgController.java
@@ -1,13 +1,9 @@
 package au.azzmosphere.pgprog.controllers;
 
 import au.azzmosphere.pgprog.models.MorseCodeModel;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.context.ApplicationContext;
 
 /**
  * Created by aaron.spiteri on 13/5/17.
@@ -15,9 +11,6 @@ import org.springframework.context.ApplicationContext;
 @Controller
 
 public class PgProgController {
-
-    @Autowired
-    private ApplicationContext applicationContext;
 
     @RequestMapping("/morsecode")
     public @ResponseBody MorseCodeModel translate() {
