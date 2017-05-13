3cc4ddea27770cfec661c076793de9671239ea1c
Aaron Spiteri <aaron.spiteri@staff.iinet.net.au>
2017-05-13 23:11:06 +1000

    Added static start page and initial application class.

---
+++ src/main/java/au/azzmosphere/pgprog/applications/Application.java
@@ -0,0 +1,13 @@
+package au.azzmosphere.pgprog.applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by aaron.spiteri on 13/5/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
